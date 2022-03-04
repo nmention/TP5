d9b86c55a252dc3e61b9a8c0f736a7209997f938
Quentin-bp <73823634+Quentin-bp@users.noreply.github.com>
2022-03-04 11:26:02 +0100

    Rajout class Graph 1ère partie

--- projet.py
+++ projet.py
@@ -1,4 +1,5 @@
 #Test
+from networkx import *
 liste = []
 
 def readFile():
@@ -7,3 +8,27 @@ def readFile():
     file.close()
 readFile()
 
+class Graphe:
+    def __init__(self,nodes):
+        self.nodes = nodes()
+        self.graph = Graph(nodes)
+
+    def CC(self,sommet):
+        cc = []
+        aTraiter = [sommet]
+        while aTraiter != []:
+            for voisins in list(self.graph.neighbors(aTraiter[0])):
+                if voisins not in cc and voisins not in aTraiter :
+                    aTraiter.append(voisins)
+            cc.append(aTraiter[0])
+            aTraiter.remove(aTraiter[0])
+            compo=Graph()
+            compo.add_nodes_from(cc)
+            for (a,b) in self.graph.edges():
+                if a in cc : 
+                    compo.add_edge(a,b) 
+            return compo
+
+
+    def isConnexe(self):
+        return len(self.graph.nodes()) == len(self.CC(self.graph,list(self.graph.nodes())[0]).nodes())
